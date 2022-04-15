47d22963e7b4b67f8a8ee9614f72df4bf5872386
Bekzodbek <97502548+Bekzodbek06@users.noreply.github.com>
2022-04-15 14:40:16 +0500

    Add files via upload

---
+++ 5ta proekt/kalkulyator.py
@@ -0,0 +1,20 @@
+sorov="ha"
+while sorov == "ha":
+    num1 = int(input("Birinchi sonni kiriting = "))
+    num2 = int(input("Ikkinchi sonni kiriting = "))
+    amal = input("Amalni kiritting : ")
+    if amal =="*":
+        print(f"{num1} {amal} {num2} = {num1*num2}")
+    elif amal =="+":
+        print(f"{num1} {amal} {num2} = {num1+num2}")
+    elif amal =="/":
+        print(f"{num1} {amal} {num2} = {num1/num2}")
+    elif amal =="-":
+        print(f"{num1} {amal} {num2} = {num1-num2}")
+    else:
+        print(f'{amal} bunday amal topilmadi ⚠ 😕')
+    sorov = input("Yana misollar bormi?😀 : ")
+    if sorov == "yoq":
+        sorov = input("Ok 😉 , \n Yana kerak bo`lsam ha buyrug`ini kiriting : ")
+    else:
+        print(f"{sorov} <= Xato, ha yoki yoq burug`idan foydalaning")
---
+++ 5ta proekt/saerch.py
@@ -0,0 +1,5 @@
+sorov = input("Qidirish uchun fayl nomini kirting : ")
+file1 = open(f"{sorov}", 'r')
+start1 = file1.read()
+print(start1)
+
