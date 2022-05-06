c7bde7fe859f041e0d78fc2df8fc005fd8fc50a2
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-05-06 13:18:13 +0300

    Update game.py

--- 5ta proekt/game.py
+++ 5ta proekt/game.py
@@ -3,18 +3,23 @@ import time as vaqt
 sorov = "ha"
 while sorov =="ha":
     compchoice = random.randint(0, 999)
+    print("Raqam tanlash uchun 0dan katta 999dan kichik son tanlang ♨  ‼")
     humanchoice = int(input("eng baland uch xonali son tanlang = "))
+    if humanchoice > 999:
+        humanchoice = 0
+    elif humanchoice < 0:
+        humanchoice = 0
     if humanchoice > compchoice:
         print(f"natijalar hioblanmoqda siz {humanchoice}ni tanladingiz bot esa {compchoice}ni tanladi")
-        vaqt.sleep(1.2)
+        vaqt.sleep(1)
         print("Siz yutdungiz")
     elif humanchoice < compchoice:
         print(f"natijalar hioblanmoqda siz {humanchoice}ni tanladingiz bot esa {compchoice}ni tanladi")
-        vaqt.sleep(1.2)
-        print(f"Siz yutqazdingiz \n Siz botdan kichik son tanladingiz bot tanlagan son {compchoice}")
+        vaqt.sleep(2)
+        print(f"Siz yutqazdingiz")
     else:
         print(f"natijalar hioblanmoqda siz {humanchoice}ni tanladingiz bot esa {compchoice}ni tanladi")
         vaqt.sleep(1)
         print("Do`stlik g`alaba qozondi")
-    sorov = input("Yana o`ynashni hohlaysizmi? ")
+    sorov = input("O`yinni to`xtatish uchun biron nima kiriting \nYana o`ynashni hohlaysizmi? ")
 print("o`yin to`xtatildi")
